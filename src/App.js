62a236078212a29d6d1f3cbb7880484e61abe25f
tjdals3759 <tjdals3759@gmail.com>
2025-03-13 21:35:57 +0900

    첫 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [글제목, b] = useState('남자 코트 추천');
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
-
+  
 export default App;
