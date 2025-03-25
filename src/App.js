6e966b316f14341269ef7457c0288a4b108d26f5
tjdals3759 <tjdals3759@gmail.com>
2025-03-25 21:54:39 +0900

    리액트 다시 시작 코딩애플 리액트 무료강의 마지막

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -5,17 +7,42 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState('남자 코트 추천');
-  
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
 
-  
+  function 함수(){
+    console.log(1);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>ReactBlog</h4>
+      </div>
+
+      <button onClick={ ()=> {
+        let copy = [...글제목];
+        copy.sort((a, b) => a.title.)
+      }}>가나다순정렬</button>
+
+      <button onClick={ ()=> {
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천'
+          글제목변경(copy);
+        }}>글수정</button>
+
+      <div className="list">
+        <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목 }</h4>
+        <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
